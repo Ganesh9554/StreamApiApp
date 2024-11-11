adc8dad86626184f3b70725ebb930224d198bc16
Ganesh9554 <ganeshroyal67@gmail.com>
2024-11-11 07:34:17 +0530

    student code updated

--- Student.java
+++ Student.java
@@ -2,6 +2,6 @@ package com.example.demo.model;
 
 public class Student {
 private Integer sId;
-private SDouble sCost;
-private String address;
+System.out.println("Hai");
+System.out.println("Bye");
 }
